8530ad433b29111290db6c58ace65e9dfb614306
Carni7vore <xanthene1893@gmail.com>
2017-11-20 20:40:05 -0600

    aux cord
    Home, party playlist, login, my picks

---
+++ AuxCord/app/src/main/java/com/example/pruales/auxcord/home_activity.java
@@ -0,0 +1,37 @@
+package com.example.pruales.auxcord;
+
+import android.app.SearchManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Created by xanth on 11/18/2017.
+ */
+public class home_activity extends AppCompatActivity {
+    protected void onCreated(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+        Toolbar toolbar= (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+    @Override
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.search1, menu);
+        MenuItem searchItem =menu.findItem(R.id.action_search);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false);
+
+        return true;
+    }
+
+
+}
---
+++ AuxCord/app/src/main/java/com/example/pruales/auxcord/party_playlist_activity.java
@@ -0,0 +1,28 @@
+package com.example.pruales.auxcord;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Created by xanth on 11/20/2017.
+ */
+
+public class party_playlist_activity extends AppCompatActivity {
+    protected void onCreated(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+        Toolbar toolbar= (Toolbar) findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.search1, menu);
+        MenuItem searchItem =menu.findItem(R.id.action_search);
+
+        return true;
+    }
+}
