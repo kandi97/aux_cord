6a677cccbee4c04e0aa11dadfe1c05b6ded284f3
Akhil Kandimalla <kandmll2@illinois.edu>
2017-11-27 13:19:55 -0600

    Persona screen added

---
+++ AuxCord/app/src/main/java/com/example/pruales/auxcord/persona_screen.java
@@ -0,0 +1,13 @@
+package com.example.pruales.auxcord;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class persona_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.persona_screen);
+    }
+}
